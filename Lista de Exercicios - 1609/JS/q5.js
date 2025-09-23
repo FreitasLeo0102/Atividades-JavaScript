abf242f2c61b0126f06bc9f2f0268d1b5d13472f
FreitasLeo0102 <freitasleo535@gmail.com>
2025-09-23 14:08:26 -0300

    Add files via upload

---
+++ Lista de Exercicios - 1609/JS/q5.js
@@ -0,0 +1,7 @@
+/* 5) Faça um programa que leia o nome e a nota de avaliação 6 filmes. Apos o cadastro de todos os filmes, exiba um menu com as opções:
+    a) Imprimir dados de 1 filme - escolher um índice e imprimir os dados
+    b) Avaliar um filme cadastrado - deve selecionar um índice e fazer a média da nova nota com a nota antiga.
+    c) Alterar dados de um filme cadatrado - deve escolher o índice de um filme para alterar seus dados.
+    d) Finalizar a execução do programa e imprimir todos os dados
+*/
+
